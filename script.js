1d17fa6eefe6322523cc9d6cf6d35b036276df19
rodVieira021 <rodvieira021@gmail.com>
2022-07-15 03:18:20 +0300

    fixed

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const btnSubmit = document.querySelector(".btn-submit");
+const btnClose = document.querySelector(".xclose");
+const form = document.querySelector(".form");
+const input = document.getElementById("input");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("clicked");
+});
