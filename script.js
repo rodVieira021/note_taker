f1f7730e44db54501187e206efdd25541340aad6
rodVieira021 <rodvieira021@gmail.com>
2022-07-21 01:00:29 +0300

    objects

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const btnSubmit = document.querySelector(".btn-submit");
 const btnClose = document.querySelector(".xclose");
 const form = document.querySelector(".form");
-const input = document.getElementById("input");
+const inputText = document.getElementById("input");
+const inputDate = document.getElementById("date");
 const message = document.querySelector(".msg");
 const notes = document.querySelector(".note");
-let data = {};
-let calendar = {};
+let data = [];
 
+// func get data
 const acceptData = () => {
-  calendar["date"] = date.value;
-  data["text"] = input.value;
+  data.push({
+    text: inputText.value,
+    date: inputDate.value,
+  });
+  console.log(data);
+
+  //   data["date"] = date.value;
+  //   data["input"] = input.value;
   createNote();
 };
 
+// form validation
 const formValidation = () => {
   if (input.value === "") {
     message.innerHTML = "Note cannot be blank!";
@@ -27,17 +35,23 @@ form.addEventListener("submit", function (e) {
   formValidation();
 });
 
-let createNote = () => {
-  notes.innerHTML += `
-        <div>
-          <h4>${calendar.date}</h4>
-          <p>${data.text}</p>
-          <button class="btn-details">View Details</button> 
-          <i onClick="deleteNote(this)" class="fa-solid fa-circle-xmark xclose"></i>
-        </div>`;
+//func creating a note
+const createNote = () => {
+  notes.innerHTML = "";
+  data.map((element, index) => {
+    notes.innerHTML += `
+              <div>
+                <h4>${element.date}</h4>
+                <p>${element.text}</p>
+                <button class="btn-details">View Details</button> 
+                <i onClick="deleteNote(this)" class="fa-solid fa-circle-xmark xclose"></i>
+                <button class="xedit">Edit</button>
+              </div>
+              `;
+  });
   input.value = "";
 };
 
-let deleteNote = (e) => {
+const deleteNote = (e) => {
   e.parentElement.remove();
 };
