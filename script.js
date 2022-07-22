8bbc07519453c9c720c6df53b3a85241cec9c5a7
rodVieira021 <rodvieira021@gmail.com>
2022-07-22 03:09:06 +0300

    modalDone

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const btnSubmit = document.querySelector(".btn-submit");
 const btnClose = document.querySelector(".xclose");
+const btnCloseMd = document.querySelector(".xclose-modal");
+const btnDetails = document.querySelectorAll(".btn-details");
+const mdContainer = document.querySelector(".modal-container");
 const form = document.querySelector(".form");
 const inputTitle = document.getElementById("title");
 const inputText = document.getElementById("input");
 const inputDate = document.getElementById("date");
 const message = document.querySelector(".msg");
+const notesAll = document.querySelectorAll(".note");
 let notes = document.querySelector(".note");
 let data = [];
 
@@ -68,7 +72,7 @@ const deleteNote = (e) => {
   e.parentElement.remove();
 };
 
-// IIFE (Immediately invoked function expression)
+// IIFE (Immediately invoked function expression) load tasks from local storage
 
 (() => {
   data = JSON.parse(localStorage.getItem("notes")) || [];
@@ -76,3 +80,20 @@ const deleteNote = (e) => {
     createNote();
   });
 })();
+
+// MODAL DETAILS
+
+notesAll.forEach((detail) => {
+  detail.addEventListener("click", function (e) {
+    e.preventDefault();
+    mdContainer.classList.remove("hidden");
+  });
+});
+
+//btn add all and target
+btnCloseMd.addEventListener("click", function (e) {
+  e.preventDefault();
+  mdContainer.classList.add("hidden");
+});
+
+//for each loop to see which detailBTN is being clicked and open the modal accordingly.
